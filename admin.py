06c56e252d24ee14bf5e9c368a791b08de28279c
PaulMilla <paul.milla1@gmail.com>
2015-07-07 07:29:36 +0000

    Registered models and formated admin page

--- admin.py
+++ admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Match, Country, Player
 
-# Register your models here.
+class CountryAdmin(admin.ModelAdmin):
+    # Explicitly choose which fields to show, how to group them, and in what order
+    fieldsets = [
+        (None,          {'fields': ['name']}),
+        ('Streaks',     {'fields': ['winning_streak', 'losing_streak']}),
+        ('Other Stats', {'fields': ['matches_played', 'top_score']}),
+    ]
+
+    list_display = ('name', 'winning_streak', 'losing_streak', 'matches_played', 'top_score')
+    list_filter = ['name', 'top_score']
+
+
+admin.site.register(Match)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Player)
