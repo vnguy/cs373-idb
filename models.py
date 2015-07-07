b7f14136b79731a9bfe44b4fc90e61cac1a43288
Jetharin Charoenworawat <kchar@cs.utexas.edu>
2015-07-07 17:49:32 -0500

    Fixed spelling error for default.

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Match(models.Model) :
     match = models.CharField(max_length=40, default="", unique=True)
     country_one = models.ForeignKey('Country', related_name='country_one', to_field='country', default='none')
     country_two = models.ForeignKey('Country', related_name='country_two', to_field='country', default='none')
-    scores = models.CharField(max_length=5, defalt="")
+    scores = models.CharField(max_length=5, default="")
     man_of_the_match = models.ForeignKey('Player', related_name='man_of_the_match', to_field='player', default='none')
     match_location = models.CharField(max_length=40, default="")
     match_date = models.CharField(max_length=40, default="")
