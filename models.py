426e6bccb9541f748a940fc9d9a9495ead9e35ba
Jetharin Charoenworawat <kchar@cs.utexas.edu>
2015-07-07 15:33:53 -0500

    Updated models.py to further reflect the scraped data.  Also manually edited some data json files to have the required attributes. A foreign key was added to country for the captain Player.

--- models.py
+++ models.py
@@ -3,58 +3,57 @@ from django.db import models
 
 class Match(models.Model) :
     """
-    The Match model contains the match name, match winner, each country's score, each country's
-    number of shots, and each country's percentage of possession.
+    The Match model contains the match name, the ending score, the man of the match,
+    the match location, and the match date.
     Two foreign keys are maintained which represent each of the two countries involved in the match.
     The __str__ method is used to retrun the name of the match.
     """
-    name = models.CharField(max_length=40, unique=True)
-    country_one = models.ForeignKey('Country', related_name='country_one', to_field='name', default='none')
-    country_two = models.ForeignKey('Country', related_name='country_two', to_field='name', default='none')
-    country_one_score = models.IntegerField(default=0)
-    country_two_score = models.IntegerField(default=0)
-    match_date = models.CharField(max_length=20)
-    match_location = models.CharField(max_length=20)
-    man_of_the_match = models.CharField(max_length=20)
+    match = models.CharField(max_length=40, unique=True)
+    country_one = models.ForeignKey('Country', related_name='country_one', to_field='country', default='none')
+    country_two = models.ForeignKey('Country', related_name='country_two', to_field='country', default='none')
+    scores = models.CharField(max_length=5)
+    man_of_the_match = models.CharField(max_length=40)
+    match_location = models.CharField(max_length=40)
+    match_date = models.CharField(max_length=40)
 
 
     def __str__(self) :
-        return self.name
+        return self.match
 
 
 
 class Country(models.Model) :
     """
-    The Country model contains the country name, number of matches played, winning streak,
-    losing streak, and top score.
+    The Country model contains the country name, number of total goals made,
+    team coach, and .
+    A foreign key is maintained for the country's captain which is a Player.
     The __str__ method is used to return the name of the country.
     """
-    name = models.CharField(max_length=30, unique=True)
-    matches_played = models.IntegerField(default=0)
-    winning_streak = models.IntegerField(default=0)
-    losing_streak = models.IntegerField(default=0)
-    top_score = models.IntegerField(default=0)
+    country = models.CharField(max_length=30, unique=True)
+    goals = models.IntegerField(default=0)
+    coach = models.CharField(max_length=40)
+    captain = models.ForeignKey('Player', related_name='captain' to_field='player', default='none')
 
 
     def __str__(self):
-        return self.name
+        return self.country
 
 
 
 class Player(models.Model) :
     """
-    The Player model contains the player name, position, number, original club, height, and weight.
+    The Player model contains the player name, country they played for, their position,
+    their dob, the number of goals they made, and their original club.
     A foreign key named country is maintained for the relation of Player to Country.
     The __str__ method is used to return the name of the player.
     """
-    name = models.CharField(max_length=30, unique=True)
-    country = models.ForeignKey(Country, related_name ='country', to_field='name', default='none')
-    position = models.CharField(max_length=1)
-    number = models.IntegerField(default=0)
-    club = models.CharField(max_length=30)
-    height = models.FloatField(default=0)
-    weight = models.IntegerField(default=0)
+    player = models.CharField(max_length=40, unique=True)
+    country = models.ForeignKey('Country', related_name ='country', to_field='country', default='none')
+    position = models.CharField(max_length=2)
+    dob = models.CharField(max_length=40)
+    goals = models.IntegerField(default=0)
+    club = models.CharField(max_length=40)
 
 
     def __str__(self):
-        return self.name
+        return self.player
