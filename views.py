37e7920315b0c32cd1c3f9d1e01634931d746d94
PaulMilla <paul.milla1@gmail.com>
2015-07-08 00:58:35 +0000

    Created a generic get page url

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# https://docs.djangoproject.com/en/1.8/intro/tutorial03/#write-views-that-actually-do-something
+
 def index (request):
     context = {}
     return render(request, 'index.html', context)
@@ -8,3 +10,7 @@ def index (request):
 def about (request):
     context = {}
     return render(request, 'about.html', context)
+
+def get_page(request, page_name):
+    context = {}
+    return render(request, page_name+".html", context)
