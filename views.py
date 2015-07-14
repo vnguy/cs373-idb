670afd6b61aa7fe023ef0dd4152eb0065d0f2f21
vnguy <vnguy@utexas.edu>
2015-07-14 17:27:06 -0500

    changed report to worddoc

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Player
+#from .models import Player
 import sqlite3
 
 db = sqlite3.connect('.../copaDB.sqlite3')
@@ -25,10 +25,11 @@ def get_page(request, page_name):
     return render(request, page_name, context)
 
 def get_player_page(request, page_name):
+    cursor.execute('''SELECT id, player, position, dob, goals FROM www_player''')
+    u = cursor.fetchone
     context = {Player.objects.get(player="page_name")}
     return render(request, page_name, context)
 
 def player_view(request):
     return 0
    # return to response('players.html', { 'players': Player.objects.all()})
-
