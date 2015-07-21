3757c2f5acaa6faa7cd45f2ff246a018a64d367e
PaulMilla <paul.milla1@gmail.com>
2015-07-21 21:28:19 +0000

    Commit and push staged changes.

--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from rest_framework import routers
-from rest_framework.urlpatterns import format_suffix_patterns
 from django.views.generic.base import RedirectView
 from . import views
 import logging
@@ -22,23 +21,20 @@ urlpatterns = [
     url(r'^$', views.get_page),
 
     # /country/
-    url(r'^country$', RedirectView.as_view(url='country/', permanent=True)),
+    url(r'^country$', RedirectView.as_view(url='country/', permanent=False)),
     url(r'^country/$', views.get_countries),
     url(r'^country/(?P<country_name>[-a-z\.A-Z]+)/{0,1}$', views.get_country),
 
     # /match/
-    url(r'^match$', RedirectView.as_view(url='match/', permanent=True)),
+    url(r'^match$', RedirectView.as_view(url='match/', permanent=False)),
     url(r'^match/$', views.get_matches),
     url(r'^match/(?P<match_name>[-a-z\.A-Z]+)$', views.get_match),
 
     # /player/
-    url(r'^player$', RedirectView.as_view(url='player/', permanent=True)),
+    url(r'^player$', RedirectView.as_view(url='player/', permanent=False)),
     url(r'^player/$', views.get_players),
     url(r'^player/(?P<player_name>[- \w]+)$', views.get_player),
 
     # /(somepage).html
     url(r'(?P<page_name>[-a-z\./A-Z]+).html$', views.get_page),
 ]
-
-#urlpatterns = format_suffix_patterns(urlpatterns)
-
